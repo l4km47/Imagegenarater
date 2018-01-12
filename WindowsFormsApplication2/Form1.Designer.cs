c16fc30f626fc4c4beb96e2c3dd944bdce32c50f
Lakmal <9912102+l4km47@users.noreply.github.com>
2018-01-12 18:17:55 +0530

    updated

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -48,6 +54,8 @@
             // 
             // backgroundWorker1
             // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             // 
@@ -68,31 +76,90 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Location = new System.Drawing.Point(65, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Location = new System.Drawing.Point(65, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Source :";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 154);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(444, 21);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(118, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(381, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 134);
+            this.ClientSize = new System.Drawing.Size(468, 182);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -109,6 +176,12 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,14 +18,27 @@ namespace WindowsFormsApplication2
         {
             InitializeComponent();
 
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            backgroundWorker1.RunWorkerAsync();
-            Hide();
+            try
+            {
+                backgroundWorker1.RunWorkerAsync(progressBar1);
+                button1.Enabled = false;
+                button2.Enabled = false;
+                progressBar1.Maximum = System.IO.Directory.GetFiles(source, "*.jpg").Length;
+                progressBar1.Value = 0;
+                button3.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private Image cropImage(string image)
         {
+
             ImageResizer resizer = new ImageResizer(image);
             // resize to 400 px, jpg quality 90
             var byteArray1 = resizer.Resize(1024, ImageEncoding.Jpg100);
@@ -44,53 +58,94 @@ namespace WindowsFormsApplication2
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-          
-            foreach (string s in System.IO.Directory.GetFiles(source, "*.jpg", System.IO.SearchOption.TopDirectoryOnly))
+            try
             {
-                //string s = "D:\\1-1000\\01 ALL-IS-WELL-2015.jpg";
-                Bitmap bitmap = (Bitmap)(cropImage(s));//load the image file                      
-                bitmap.SetResolution(20, 20);
-
-                using (Graphics graphics = Graphics.FromImage(bitmap))
+                foreach (string s in System.IO.Directory.GetFiles(source, "*.jpg"))
                 {
-                    using (Font arialFont = new Font("Arial", 150))
+                    //string s = "D:\\1-1000\\01 ALL-IS-WELL-2015.jpg";
+                    Bitmap bitmap = (Bitmap)(cropImage(s));//load the image file                      
+                    bitmap.SetResolution(20, 20);
+
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
                     {
-                        PointF firstLocation = new PointF(bitmap.Width - 90, 25);
-                        System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(Color.Red);
-                        graphics.FillEllipse(myBrush, new Rectangle(bitmap.Width - 105, 5, 100, 100));
-                        string[] words = System.IO.Path.GetFileName(s).Split(' ');
-                        graphics.DrawString(words[0], arialFont, Brushes.Black, firstLocation);
-                        myBrush.Dispose();
+                        using (Font arialFont = new Font("Arial", 150))
+                        {
+                            PointF firstLocation = new PointF(bitmap.Width - 90, 25);
+                            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(Color.Red);
+                            graphics.FillEllipse(myBrush, new Rectangle(bitmap.Width - 105, 5, 100, 100));
+                            string[] words = System.IO.Path.GetFileName(s).Split(' ');
+                            graphics.DrawString(words[0], arialFont, Brushes.Black, firstLocation);
+                            myBrush.Dispose();
+                        }
                     }
+                    bitmap.Save(target + "\\" + System.IO.Path.GetFileName(s));
+                    Invoke(new Action(() => label6.Text = "Current file > " + System.IO.Path.GetFileName(s)));
+                    bitmap.Dispose();
+                    string del = Application.StartupPath + "\\" + System.IO.Path.GetFileName(s);
+                    System.IO.File.Delete(del);
+                    var p = (ProgressBar)e.Argument;
+                    Invoke(new Action(() => p.Increment(1)));
                 }
-                bitmap.Save(target + "\\" + System.IO.Path.GetFileName(s));
-                bitmap.Dispose();
-                string del = Application.StartupPath + "\\" + System.IO.Path.GetFileName(s);
-                System.IO.File.Delete(del);
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Invoke(new Action(() => button3.Enabled = false));
+            }
+
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            Show();
-            MessageBox.Show("OK");
+            try
+            {
+                MessageBox.Show("OK", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                progressBar1.Value = 0;
+
+                System.IO.StreamWriter st = new System.IO.StreamWriter(target + "\\names.txt", false, Encoding.UTF8);
+
+                foreach (string s in System.IO.Directory.GetFiles(source, "*.jpg", System.IO.SearchOption.TopDirectoryOnly))
+                {
+                    st.WriteLine(System.IO.Path.GetFileName(s));
+                }
+                st.Flush();
+                st.Dispose();
+                label6.Text = "";
+                button3.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            folderBrowserDialog1.Description = "Select source path";
-
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            try
             {
-                source = folderBrowserDialog1.SelectedPath;
-                label1.Text = source;
-                folderBrowserDialog1.Description = "Select Export path";
+                folderBrowserDialog1.Description = "Select source path";
 
                 if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    target = folderBrowserDialog1.SelectedPath;
-                    label2.Text = target;
+                    source = folderBrowserDialog1.SelectedPath;
+                    label1.Text = source;
+
+                    label5.Text = String.Format("Images in Source folder {0}", System.IO.Directory.GetFiles(source, "*.jpg").Length.ToString());
+
+                    folderBrowserDialog1.Description = "Select Export path";
+
+                    if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        target = folderBrowserDialog1.SelectedPath;
+                        label2.Text = target;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -102,6 +157,11 @@ namespace WindowsFormsApplication2
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
         }
     }
 }
